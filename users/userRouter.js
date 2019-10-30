9cadaa25db3a5432e5268b39ef852cd09a983c0f
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-30 16:57:49 -0400

    validateUser middle ware is functional

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const db = require('./userDb.js');
+const postdb = require("../posts/postDb.js")
 
+router.post('/', validateUser, (req, res) => {
+    const body = req.body
+    db.insert(body)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        res.status(500).json({ error: ".post '/' error" })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -33,15 +43,42 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    let id = req.params.id;
+    db.getById(id)
+    .then(user => {
+        if(user){
+            next();
+        } else {
+            res.status(400).json({ message: "invalid user id" })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ you:"shall not pass!!" })
+    })
 };
 
 function validateUser(req, res, next) {
-
+    const body = req.body;
+    const name = req.body.name;
+    if(!body){
+        res.status(400).json({ message: "missing user data" })
+    } else if(!name){
+        res.status(400).json({ message: "missing required name field" })
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
-
+    const body = req.body;
+    const text = req.body.text;
+    if(!body){
+        res.status(400).json({ message: "missing post data" })
+    } else if(!text){
+        res.status(400).json({ message: "missing required text field" })
+    } else {
+        next();
+    }
 };
 
 module.exports = router;
