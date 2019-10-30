0e1e940a8af142b94cf85f85a5c1373901d7370d
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-30 17:32:06 -0400

    everything functional and tested!

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,36 +8,95 @@ const postdb = require("../posts/postDb.js")
 router.post('/', validateUser, (req, res) => {
     const body = req.body
     db.insert(body)
-    .then(user => {
-        res.status(200).json(user);
+    .then(users => {
+        res.status(200).json(users);
     })
     .catch(error => {
         res.status(500).json({ error: ".post '/' error" })
     })
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', [validateUserId, validatePost], (req, res) => {
+    const body = req.body;
+    postdb.insert(body)
+    .then(posts => {
+        res.status(201).json(posts);
+    })
+    .catch(error =>{
+        res.status(500).json({ error: ".post '/:id/posts' error" })
+    })
 });
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        res.status(500).json({ error: ".get / error" })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId,(req, res) => {
+    const id = req.params.id;
+    db.getById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        res.status(500).json({ error: ".get /:id error" })
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId,(req, res) => {
+    const id = req.params.id;
+    db.getUserPosts(id)
+    .then(posts => {
+      res.status(201).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error getting user's posts."
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId,(req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The user has been deleted."
+        });
+      } else {
+        res.status(404).json({
+          message: "The user could not be found."
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error deleting the user."
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', [validateUser, validateUserId],(req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    db.update(id, body)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          message: "Error updating user"
+        });
+      });
 });
 
 //custom middleware
@@ -74,7 +133,7 @@ function validatePost(req, res, next) {
     const text = req.body.text;
     if(!body){
         res.status(400).json({ message: "missing post data" })
-    } else if(!text){
+    } if(!text){
         res.status(400).json({ message: "missing required text field" })
     } else {
         next();
